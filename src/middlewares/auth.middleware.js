ff8394dde8cbf7b62c6048170da4d1c8f71755b3
Raj143 <rajshingade777@gmail.com>
2024-05-10 17:57:53 +0530

    [Raj] add : checking that if and only if admin is there then it is working otherwise not

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,27 +3,24 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-/**
- * Middleware to authenticate if user has a valid Authorization token
- * Authorization: Bearer <token>
- *
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- */
 export const userAuth = async (req, res, next) => {
   try {
     let bearerToken = req.header('Authorization');
+
     if (!bearerToken)
       throw {
         code: HttpStatus.BAD_REQUEST,
         message: 'Authorization token is required'
       };
     bearerToken = bearerToken.split(' ')[1];
-
     const { user } = await jwt.verify(bearerToken, process.env.SECRET_KEY);
     res.locals.user = user;
-    res.locals.token = bearerToken;
+    if (user.role !== 'admin') {
+      throw {
+        code: HttpStatus.FORBIDDEN,
+        message: 'Access forbidden. User does not have the required role.'
+      };
+    }    res.locals.token = bearerToken;
     next();
   } catch (error) {
     next(error);
